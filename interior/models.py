b3a10cb126d1b913392a12d3f7f83b51c62e8dba
Umid <umidrifkatov@gmail.com>
2022-11-12 17:29:25 +0300

    r

---
+++ interior/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=300)
+    shorttitle = models.CharField(max_length=300)
+
+    category = models.ForeignKey()
+    author = models.ForeignKey()
+
+    locationcity = models.CharField()
+    loactionplace = models.CharField()
+    locationpic = models.CharField()
+    
+    shortdesc = models.CharField(max_length=300)
+    longdesc = models.CharField(max_length=300)
+
+    startdate = models.CharField(max_length=300)
+    enddate = models.CharField(max_length=300)
+
+    price = models.CharField()
+
+
+
