77d62cb641cb13dd0c7cb33a7d31baa8fed043c9
Oleg <umidrifkatov@gmail.com>
2022-11-13 06:19:48 +0300

    'r'

--- interior/models.py
+++ interior/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 
 
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=300)
+
+
+
+
 class Course(models.Model):
     title = models.CharField(max_length=300)
-    # shorttitle = models.CharField(max_length=300)
+    shorttitle = models.CharField(max_length=300)
 
-    # category = models.ForeignKey()
-    # author = models.ForeignKey()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
 
-    # locationcity = models.CharField()
-    # loactionplace = models.CharField()
-    # locationpic = models.CharField()
+    locationcity = models.CharField()
+    loactionplace = models.CharField()
+    locationpic = models.CharField()
     
-    # shortdesc = models.CharField(max_length=300)
-    # longdesc = models.CharField(max_length=300)
+    shortdesc = models.CharField(max_length=300)
+    longdesc = models.CharField(max_length=300)
 
-    # startdate = models.CharField(max_length=300)
-    # enddate = models.CharField(max_length=300)
+    startdate = models.CharField(max_length=300)
+    enddate = models.CharField(max_length=300)
 
-
-    # price = models.CharField()
+    price = models.IntegerField()
 
 
 
